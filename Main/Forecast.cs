fb8b1e7763a3b5f4e29373c5214e6277979939eb
Dmitrii Ershov <dmitryershov0@gmail.com>
2020-07-26 10:13:49 +0300

    Initial project

---
+++ Main/Forecast.cs
@@ -0,0 +1,50 @@
+﻿using Accuweather.Core.Helpers;
+using Accuweather.Core;
+using System.Threading.Tasks;
+
+namespace Accuweather.Forecast
+{
+	/// <summary>
+	///	Accuweather Forecast Api
+	/// </summary>
+	public class Forecast : AccuweatherApiCore, IForecast
+	{
+		private const string _baseUrl = "http://dataservice.accuweather.com/forecasts/v1";
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="apiKey">Api key accuweather.</param>
+		/// <param name="language">Language.</param>
+		/// <returns></returns>
+		public Forecast(string apiKey, string language = "en-us") : base(apiKey, language)
+		{
+
+		}
+		private object GetObjectForecasts(bool details, bool metric)
+		{
+			return new {
+				language = _language,
+				details,
+				metric
+			};
+		}
+		public async Task<string> FiveDaysOfDailyForecasts(int locationKey, bool details = false,
+		 bool metric = false)
+		{
+			var obj = GetObjectForecasts(details, metric);
+			var url = $"{_baseUrl}/daily/5day/{locationKey}?apikey={_apiKey}&";
+
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		public async Task<string> TwelveHoursOfHourlyForecasts(int locationKey, bool details = false,
+		 bool metric = false)
+		{
+			var obj = GetObjectForecasts(details, metric);
+			var url = $"{_baseUrl}/hourly/12hour/{locationKey}?apikey={_apiKey}&";
+
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}		
+	}
+}
---
+++ Main/IForecast.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+namespace Accuweather.Forecast
+{
+	/// <summary>
+	/// Get forecast information for a specific location..
+	/// </summary>
+	public interface IForecast
+	{
+		/// <summary>
+		/// Returns an array of daily forecasts for the next 5 days for a specific location.
+		/// Forecast searches require a location key.
+		/// </summary>
+		/// <param name="locationKey">Unique ID to search for a specific location.</param>
+		/// <param name="details">Boolean value specifies whether or not to include full details in the response.</param>
+		/// <param name="metric">Boolean value specifies whether or not to display metric values.</param>
+		/// <returns></returns>
+		Task<string> FiveDaysOfDailyForecasts(int locationKey, bool details = false,
+		 bool metric = false);
+
+		/// <summary>
+		/// Returns an array of hourly forecasts for the next 12 hours for a specific location.
+		/// Forecast searches require a location key. 
+		/// </summary>
+		/// <param name="locationKey">Unique ID to search for a specific location.</param>
+		/// <param name="details">Boolean value specifies whether or not to include full details in the response.</param>
+		/// <param name="metric">Boolean value specifies whether or not to display metric values.</param>
+		/// <returns></returns>
+		Task<string> TwelveHoursOfHourlyForecasts(int locationKey, bool details = false,
+		bool metric = false);
+	}
+}
